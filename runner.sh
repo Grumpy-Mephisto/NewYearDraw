56aace8201cce95e06b09f4ec53cffcc3486c585
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-12-21 18:25:47 +0700

    🧹 chore: add script for run project

---
+++ runner.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Define colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Check if java is installed
+if ! command -v java &> /dev/null; then
+    echo -e "${RED}Error: Java is not installed.${NC}" >&2
+    exit 1
+fi
+
+# Check if Main.java exists
+if [ ! -f "Main.java" ]; then
+    echo -e "${RED}Error: Main.java not found.${NC}" >&2
+    exit 1
+fi
+
+# Compile Java files
+if ! javac *.java; then
+    echo -e "${RED}Compilation failed.${NC}" >&2
+    exit 1
+fi
+
+# Run Main class
+java Main
+
+# Check the exit status and display success/failure message
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}Success${NC}"
+else
+    echo -e "${RED}Failed${NC}"
+fi
