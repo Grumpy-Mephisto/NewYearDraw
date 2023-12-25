788bb28d4d35a497a95261559d20af5a52940651
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-12-25 08:51:54 +0700

    🧹 chore: remove useless args

--- runner.sh
+++ runner.sh
@@ -7,19 +7,19 @@ NC='\033[0m' # No Color
 
 # Check if java is installed
 if ! command -v java &> /dev/null; then
-    echo -e "${RED}Error: Java is not installed.${NC}" >&2
+    echo "${RED}Error: Java is not installed.${NC}" >&2
     exit 1
 fi
 
 # Check if Main.java exists
 if [ ! -f "Main.java" ]; then
-    echo -e "${RED}Error: Main.java not found.${NC}" >&2
+    echo "${RED}Error: Main.java not found.${NC}" >&2
     exit 1
 fi
 
 # Compile Java files
 if ! javac *.java; then
-    echo -e "${RED}Compilation failed.${NC}" >&2
+    echo "${RED}Compilation failed.${NC}" >&2
     exit 1
 fi
 
