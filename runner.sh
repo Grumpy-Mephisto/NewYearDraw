08d5e5e643e4bdd316b8bd2dcd7b50c62db38359
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-12-25 08:59:37 +0700

    🧹 chore: make runner script better structure

--- runner.sh
+++ runner.sh
@@ -5,26 +5,43 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-# Check if java is installed
-if ! command -v java &> /dev/null; then
-    echo "${RED}Error: Java is not installed.${NC}" >&2
-    exit 1
-fi
+check_java() {
+    # Check if Java is installed
+    if ! command -v java &> /dev/null; then
+        echo "${RED}Error: Java is not installed.${NC}" >&2
+        exit 1
+    fi
+}
 
-# Check if Main.java exists
-if [ ! -f "Main.java" ]; then
-    echo "${RED}Error: Main.java not found.${NC}" >&2
-    exit 1
-fi
+check_main_file() {
+    # Check if Main.java exists
+    if [ ! -f "Main.java" ]; then
+        echo "${RED}Error: Main.java not found.${NC}" >&2
+        exit 1
+    fi
+}
 
-# Compile Java files
-if ! javac *.java; then
-    echo "${RED}Compilation failed.${NC}" >&2
-    exit 1
-fi
+compile_java_files() {
+    # Compile Java files
+    if ! javac *.java; then
+        echo "${RED}Compilation failed.${NC}" >&2
+        exit 1
+    fi
+}
 
-# Run Main class
-java Main
+run_main_class() {
+    # Run Main class
+    java Main
+}
 
-# Remove all generated .class files recursively
-find . -name '*.class' -exec rm -f {} +
+cleanup() {
+    # Remove all generated .class files recursively
+    find . -name '*.class' -exec rm -f {} +
+}
+
+# Execution sequence
+check_java
+check_main_file
+compile_java_files
+run_main_class
+cleanup
