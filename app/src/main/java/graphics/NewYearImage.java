01988c00a7ab16791d242f28c92781781d2ae9b6
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-12-28 14:06:56 +0700

    🎨 style: move func star to under func tree

--- app/src/main/java/graphics/NewYearImage.java
+++ app/src/main/java/graphics/NewYearImage.java
@@ -8,12 +8,12 @@ import javax.swing.*;
 public class NewYearImage extends JPanel {
     private Snow Snow = new Snow();
     private Tree ChristmasTree = new Tree();
+    private Star Star = new Star();
     private Snowman Snowman = new Snowman();
     private Confetti Confetti = new Confetti();
-    private Star Star = new Star();
     // private Elsa Elsa = new Elsa();
 
-    private String[] BackgroundColors = {"#0c1445", "#2d2351", "#4c408e"};
+    private String[] BackgroundColors = { "#0c1445", "#2d2351", "#4c408e" };
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -24,9 +24,9 @@ public class NewYearImage extends JPanel {
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         // Background
-        Color[] color = {Color.decode(BackgroundColors[0]), Color.decode(BackgroundColors[1]),
-                Color.decode(BackgroundColors[2])};
-        float[] BackgroundDist = {0.0f, 0.5f, 1.0f};
+        Color[] color = { Color.decode(BackgroundColors[0]), Color.decode(BackgroundColors[1]),
+                Color.decode(BackgroundColors[2]) };
+        float[] BackgroundDist = { 0.0f, 0.5f, 1.0f };
         g2d.setPaint(new LinearGradientPaint(getWidth() / 2, 0, getWidth() / 2, getHeight(),
                 BackgroundDist, color));
         g2d.fillRect(0, 0, getWidth() + 1, getHeight() + 1);
@@ -37,6 +37,9 @@ public class NewYearImage extends JPanel {
         // Tree
         ChristmasTree.draw(g2d, getWidth() + 400, getHeight() + 100);
 
+        // Star
+        Star.draw(g2d, getWidth() + 400, getHeight() - 425, 200);
+
         // Snowman
         Snowman.draw(g2d, getWidth() - 400, getHeight() + 100);
 
@@ -45,8 +48,5 @@ public class NewYearImage extends JPanel {
 
         // Elsa
         // Elsa.draw(g2d, getWidth(), getHeight()); // illegal ways
-
-        // Star
-        Star.draw(g2d, getWidth() + 400, getHeight() - 425, 200);
     }
 }
